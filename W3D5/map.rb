78134ae021add0ae006acabb1e62f4dc2f2a46ec
Kathy Vu <vu.kathy98@gmail.com>
2023-02-17 11:33:55 -0800

    Change map array to hash

--- W3D5/map.rb
+++ W3D5/map.rb
@@ -1,39 +1,30 @@
-class Map
+class MyMap
     def initialize
-        # @map = []
         @map = {}
     end
 
     def set(key, value)
-        @map[key] = value
-        # (0...@map.length).each do |i|
-        #     @map[i][1] = value if @map[i][0] == key
-        # end
-        # @map << [key, value] if @map.none? {|pair| pair[0] == key}
+        map[key] = value
+        self
     end
 
     def get(key)
-        @map[key]
-        # return nil if @map.none? {|pair| pair[0] == key}
-        # (0...@map.length).each do |i|
-        #     return @map[i][1] if @map[i][0] == key
-        # end
+        map[key]
     end
 
     def delete(key)
-        @map.delete_if {|k, v| k == key }   #=> {"a"=>100}
-
-        # del_idx = @map.find_index { |k,_| k == key } #=> 1
-        # @map.delete(del_idx)
-        # @map.each_with_index {|pair, i| @map.delete(@map[i]) if pair[0] == key }
+        map.delete_if {|k, v| k == key }   #=> {"a"=>100}
     end
 
     def show
-        @map
+        return map.dup
     end
+
+    private 
+    attr_reader :map
 end
 
-p map = Map.new
+p map = MyMap.new
 puts "add k-v pair"
 p map.set(3, 5)
 # p map.show
@@ -42,3 +33,4 @@ p map.show
 p map.set(4, 8)
 p map.delete(3)
 p map.show
+p map.get(4)
