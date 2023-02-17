02b4c9bae093c10aaa809a63a4f916e117272dbe
Kathy Vu <vu.kathy98@gmail.com>
2023-02-17 00:31:16 -0800

    Add W3D5 folder and stack.rb file

---
+++ W3D5/stack.rb
@@ -0,0 +1,27 @@
+class Stack
+    def initialize
+      # create ivar to store stack here!
+      @stack = []
+    end
+  
+    def push(el)
+      # adds an element to the stack
+      @stack << el
+    end
+  
+    def pop
+      # removes one element from the stack
+      @stack.delete(@stack[0])
+    end
+  
+    def peek
+      # returns, but doesn't remove, the top element in the stack
+      @stack[0]
+    end
+end
+
+stk = Stack.new
+p stk.push(3)
+p stk.peek
+p stk.pop
+p stk
